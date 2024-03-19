ceb9d94d2f8aade327991784c36752ba14d12397
IT21314742 <it21314742@my.sliit.lk>
2024-03-19 20:21:10 +0530

    Update JS
    add js

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 var selectorField = document.getElementById("selectorField");
 var selectText = document.getElementById("selectText");
-var options = document.getElementsByClassName("options")
+var options = document.getElementsByClassName("options");
