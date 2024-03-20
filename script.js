87e9a26f1055b55322b9f1a73634a71d626857e8
IT21314742 <it21314742@my.sliit.lk>
2024-03-20 09:31:24 +0530

    Update JS
    Update JS

--- script.js
+++ script.js
@@ -4,12 +4,12 @@ var options = document.getElementsByClassName("options");
 var list = document.getElementById("list");
 var arrowIcon = document.getElementById("arrowIcon")
 
-selectorField.onclick = function(){
+selectorField.onclick = function () {
     list.classList.toggle("hide");
     arrowIcon.classList.toggle("rotate");
 }
-for(Option of options){
-    Option.onclick = function(){
+for (Option of options) {
+    Option.onclick = function () {
         selectText.innerHTML = this.textContent;
         list.classList.toggle("hide");
         arrowIcon.classList.toggle("rotate");
