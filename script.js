911fb1b5926b9f9690c9c08bef66cec892080a92
IT21314742 <it21314742@my.sliit.lk>
2024-03-20 09:29:23 +0530

    Update JS
    add js

--- script.js
+++ script.js
@@ -11,6 +11,6 @@ selectorField.onclick = function(){
 for(Option of options){
     Option.onclick = function(){
         selectText.innerHTML = this.textContent;
-        list.classList
+        list.classList.toggle
     }
 }
